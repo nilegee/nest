989812be1bb6e9e1768acdf1488a384f221011d9
nilegee <nilezat@gmail.com>
2025-08-16 01:21:57 +0400

    Update env.js

--- web/env.js
+++ web/env.js
@@ -3,13 +3,13 @@
  * DO NOT EDIT MANUALLY - Run `node scripts/sync-env.mjs` to update
  */
 
-export const SUPABASE_URL = 'https://zlhamcofzyozfyzcgcdg.supabase.co';
-export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpsaGFtY29menlvemZ5emNnY2RnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM5NTM0MjIsImV4cCI6MjA2OTUyOTQyMn0.CqMDQgfpbyWTi3RgA_eitd_Qf7aJu0WruETtws6B5Mo';
+export const SUPABASE_URL = 'https://cqloirfwewoncyiyvjnz.supabase.co';
+export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNxbG9pcmZ3ZXdvbmN5aXl2am56Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUyODk3OTEsImV4cCI6MjA3MDg2NTc5MX0.yxoN46QgxoeXzyaoMkDo6MYPn2lJzpgMLIj_ixVfPdY';
 export const WHITELISTED_EMAILS = ['yazidgeemail@gmail.com', 'yahyageemail@gmail.com', 'abdessamia.mariem@gmail.com', 'nilezat@gmail.com'];
 
 export const ENV = {
-  "SUPABASE_URL": "https://zlhamcofzyozfyzcgcdg.supabase.co",
-  "SUPABASE_ANON_KEY": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpsaGFtY29menlvemZ5emNnY2RnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM5NTM0MjIsImV4cCI6MjA2OTUyOTQyMn0.CqMDQgfpbyWTi3RgA_eitd_Qf7aJu0WruETtws6B5Mo",
+  "SUPABASE_URL": "https://cqloirfwewoncyiyvjnz.supabase.co",
+  "SUPABASE_ANON_KEY": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNxbG9pcmZ3ZXdvbmN5aXl2am56Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUyODk3OTEsImV4cCI6MjA3MDg2NTc5MX0.yxoN46QgxoeXzyaoMkDo6MYPn2lJzpgMLIj_ixVfPdY",
   "WHITELISTED_EMAILS": "yazidgeemail@gmail.com,yahyageemail@gmail.com,abdessamia.mariem@gmail.com,nilezat@gmail.com",
   "DEBUG": false
 };
