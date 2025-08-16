8fdfcb4d6cec0fbf2945e362f89667cf25cc7273
nilegee <nilezat@gmail.com>
2025-08-16 18:09:53 +0400

    Create 20250816000599_drop_old_policies.sql

---
+++ supabase/migrations/20250816000599_drop_old_policies.sql
@@ -0,0 +1,5 @@
+drop policy if exists families_read_allowed_users on families;
+drop policy if exists profiles_read_allowed_users on profiles;
+drop policy if exists events_family_read on events;
+drop policy if exists posts_family_read on posts;
+drop policy if exists islamic_guidance_read_all on islamic_guidance;
